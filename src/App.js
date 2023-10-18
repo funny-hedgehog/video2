ad57fe98a21c9197f8fadcbe2c4b0d25eeb40c0c
arifm <arifmho@yandex.ru>
2023-10-18 15:47:42 +0300

    firstly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [error, setError] = React.useState()
+  const [isEnabled,setEnabled] = React.useState(false)
+  const videoRef = React.useRef(null)
+  const streamRef = React.useRef(null)
+
+  const startStream = () => {
+    navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    })
+        .then((stream) => {
+          videoRef.current.srcObject = stream;
+          videoRef.current.onloadedmetadata = () => videoRef.current.play();
+        })
+        .catch(err => {
+          setError(err.name)
+        });
+  }
+
+  const stopStream = () => {
+    if (streamRef.current) {
+      streamRef.current.getTracks().forEach((track) => track.stop());
+    }
+  }
+
+  React.useEffect(() => {
+    setError(null)
+    stopStream()
+    if (isEnabled) startStream()
+  }, [isEnabled])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        {isEnabled && (
+            <>
+              <video playsInline muted autoPlay ref={videoRef}/>
+              {error && <div className="error">{error}</div>}
+            </>
+        )}
+        <div className='controls'>
+          <button onClick={() => setEnabled(!isEnabled)}>
+            {isEnabled ? 'Off' : 'ON'}
+          </button>
+        </div>
+      </>
   );
 }
 
